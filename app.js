0040c7d110b74224bc856613f1de2e7b9956d197
murza-08 <ergeshovmurza.08@gmail.com>
2024-10-11 11:57:59 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// // const num = 10
+// // const num1 = 50 
+// // console.log(num/num1)
+// // console.log(num-num1)
+// // console.log(num*num1)
+// const num = 100
+// const num2 = '200'
+// console.log(num + Number (num2))
+const num = prompt('сан жаз')
+const num2 = prompt(' дагы сан жаз')
+alert( Number(num +)  Number( num2));
+
